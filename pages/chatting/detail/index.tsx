4a0560b0ebd200de73e8fd37ac651a4a364b2f18
Dalaran <minjiwr123@gmail.com>
2022-03-20 19:52:45 +0900

    design/#30: 로그인 UI

--- pages/chatting/detail/index.tsx
+++ pages/chatting/detail/index.tsx
@@ -1,6 +1,5 @@
 import ChattingDetail from "../../../src/components/units/chatting/detail/chattingDetail.container";
-import ChattingDetailPropose from "../../../src/components/units/chatting/detail/detailPropose/chattingDetailPropose.container";
 
-export default function ChattingDetailPage(){
-    return <ChattingDetail/>
+export default function ChattingDetailPage() {
+  return <ChattingDetail />;
 }
---
+++ src/components/units/login/login.types.ts
@@ -0,0 +1,5 @@
+export interface IPropsLoginUI {
+  type: string;
+  isView: boolean;
+  onClickSetIsView: () => void;
+}
