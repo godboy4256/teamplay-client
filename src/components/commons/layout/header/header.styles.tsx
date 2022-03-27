15e1c3d2935f3372b5352fa2efeee5af8f618cbd
godboy4256 <godboy4256@github.com>
2022-03-27 20:49:15 +0900

    design-#85: 헤더 반응형 PC 적용, 반응형 작업에 따른 TSX 구조 변경

--- src/components/commons/layout/header/header.styles.tsx
+++ src/components/commons/layout/header/header.styles.tsx
@@ -2,15 +2,44 @@ import styled from "@emotion/styled";
 
 
 export const HeaderStyle = styled.header`
-    padding: 0 20px;
+    background-color: #fff;
+    width: 100%;
+    display: flex;
+    justify-content: center;
     border-bottom: 1px solid #ccc;
+    /* position: fixed;
+    top:0;
+    left:0;
+    z-index: 100; */
+    & > div{
+        width: 100%;
+        max-width: 1200px;
+        padding: 0 20px;
+        display: flex;
+        justify-content: space-between;
+    }
 `
 
 export const HeaderTop = styled.div`
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    padding: 12px 0;
+    flex-direction: column;
+    & h1{
+        padding: 12px 0;
+        @media (min-width: 1200px) {
+            padding:20px 0;
+            margin-right: 50px;
+        }
+        & img{
+            @media (min-width: 1200px) {
+                width:12.643rem
+            }
+        }
+    };
+    @media (min-width: 1200px) {
+        flex-direction: row;
+        align-items: center;
+    }
 `
 
 export const HeaderNav = styled.nav`
@@ -22,17 +51,21 @@ export const HeaderNav = styled.nav`
         cursor: pointer;
         position: relative;
         padding: 12px 0;
+        @media (min-width: 1200px) {
+            font-size:  18px;
+            padding: 23px 0;
+        }
    }
    & ul>li.nav__list.active__page{
        ::after{
-        display:block;
-        content: " ";
-        width: 100%;
-        height: 1px;
-        background-color: #3894FF;
-        position: absolute;
-        bottom: -1px;
-        left: 0;
+            display:block;
+            content: " ";
+            width: 100%;
+            height: 2px;
+            background-color: #3894FF;
+            position: absolute;
+            bottom: -1px;
+            left: 0;
        }
    }
    color: #9D9D9D;
@@ -49,8 +82,21 @@ export const Logo = styled.div`
 
 export const Setting = styled.div` 
     line-height: 0;
+    padding-top: 12px;
+    display: flex;
+    align-items: center;
     & button{
         line-height: 0;
         margin-left: 7px;
+    }
+    & .login-signup{
+        display: none;
+        font-size: 17px;
+        @media (min-width: 1200px) {
+            display: block;
+        }
+    }
+    @media (min-width: 1200px) {
+        padding-top: 0;
     }
 `
