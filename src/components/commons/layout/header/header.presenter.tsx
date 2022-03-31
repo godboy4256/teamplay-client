16adbadf544e5d75372e8781c2c697a082363973
Dalaran <minjiwr123@gmail.com>
2022-03-31 16:23:36 +0900

    feat-#116: header 작업 :sparkles: 1. 모바일 햄버거 토글 기능, 2. 웹 햄버거 토글 반응형, 3. 로그인 상태따른 헤더 UI 변경, 4. routin 설정, 5. logout 기능

--- src/components/commons/layout/header/header.presenter.tsx
+++ src/components/commons/layout/header/header.presenter.tsx
@@ -1,63 +1,84 @@
+import { useContext } from "react";
+import useFetchUser from "../../hooks/useFetchUser";
 import useMoveToPage from "../../hooks/useMoveToPage";
+import { HeaderContext } from "./header.container";
 import * as S from "./header.styles";
 import { IPropsHeaderUI } from "./header.types";
+import HeaderToggle from "./toggle/headerToggle";
 
 export default function HeaderUI(props: IPropsHeaderUI) {
   const { onClickHeaderRouting } = useMoveToPage();
+  const { onCliclsetPosition, onClickMovetoPage } = useContext(HeaderContext);
+  const { data } = useFetchUser();
   return (
     <S.HeaderStyle>
-      <div>
-      <S.HeaderTop>
-        <h1>
-          <S.Logo>
-            <img src="/img/commons/logo.svg" alt="logo" />
-          </S.Logo>
-        </h1>
-        {!props.isHiddenNav && (
-        <S.HeaderNav>
-          <ul>
-            <li onClick={onClickHeaderRouting} id="/main" className="nav__list">
-              홈
-            </li>
-            <li
-              onClick={onClickHeaderRouting}
-              id="/project/list"
-              className="nav__list"
-            >
-              프로젝트
-            </li>
-            <li
-              onClick={onClickHeaderRouting}
-              id="/profile"
-              className="nav__list"
-            >
-              팀원
-            </li>
-            <li
-              onClick={onClickHeaderRouting}
-              id="/community/list"
-              className="nav__list"
-            >
-              커뮤니티
-            </li>
-          </ul>
-        </S.HeaderNav>
-      )}
-      </S.HeaderTop>
+      <S.HeaderWrapper>
+        <S.HeaderTop>
+          <h1>
+            <S.Logo>
+              <img src="/img/commons/logo.svg" alt="logo" />
+            </S.Logo>
+          </h1>
+          {!props.isHiddenNav && (
+            <S.HeaderNav>
+              <ul>
+                <li
+                  onClick={onClickHeaderRouting}
+                  id="/main"
+                  className="nav__list"
+                >
+                  홈
+                </li>
+                <li
+                  onClick={onClickHeaderRouting}
+                  id="/project/list"
+                  className="nav__list"
+                >
+                  프로젝트
+                </li>
+                <li
+                  onClick={onClickHeaderRouting}
+                  id="/profile"
+                  className="nav__list"
+                >
+                  팀원
+                </li>
+                <li
+                  onClick={onClickHeaderRouting}
+                  id="/community/list"
+                  className="nav__list"
+                >
+                  커뮤니티
+                </li>
+              </ul>
+            </S.HeaderNav>
+          )}
+        </S.HeaderTop>
         <S.Setting>
-          <button className="login-signup">
-            로그인 / 회원가입
-          </button>
-          <div className="user-arvata">
-          </div>
+          {data ? (
+            <S.UserArvata>
+              <img src="/img/commons/defaultProfile.jpg" />
+            </S.UserArvata>
+          ) : (
+            <div className="login-signup">
+              <span onClick={onClickMovetoPage && onClickMovetoPage("login")}>
+                로그인
+              </span>{" "}
+              <span> / </span>{" "}
+              <span onClick={onClickMovetoPage && onClickMovetoPage("signup")}>
+                회원가입
+              </span>
+            </div>
+          )}
           <button className="alam">
             <img src="/img/alam.svg" />
           </button>
-          <button>
-            <img src="/img/btn.svg" />
-          </button>
+          <div className="toggle">
+            <HeaderToggle />
+            <img src="/img/btn.svg" onClick={onCliclsetPosition} />
+          </div>
         </S.Setting>
-        </div>
+      </S.HeaderWrapper>
     </S.HeaderStyle>
   );
 }
---
+++ src/components/commons/layout/header/toggle/headerToggle.styles.ts
@@ -0,0 +1,156 @@
+import styled from "@emotion/styled";
+import { IPropsHeaderToggleWrapper } from "../header.types";
+import { breakPoints } from "../../../../../commons/styles/breakpoint";
+
+export const Wrapper = styled.div`
+  width: 80vw;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: ${(props: IPropsHeaderToggleWrapper) => `${props.position}vw`};
+  transition: all 0.25s;
+  z-index: 2;
+  background-color: #fff;
+  border: 1px solid #e9e9e9;
+  border-radius: 12px;
+
+  @media ${breakPoints.tablet} {
+    display: ${(props: IPropsHeaderToggleWrapper) =>
+      props.isView ? "block" : "none"};
+    height: auto;
+    position: absolute;
+    top: 2vw;
+    right: 0;
+    width: 350px;
+  }
+
+  @media ${breakPoints.web} {
+    display: ${(props: IPropsHeaderToggleWrapper) =>
+      props.isView ? "block" : "none"};
+    height: auto;
+    position: absolute;
+    top: 2vw;
+    right: 0;
+    width: 350px;
+    box-shadow: 0px 0px 20px rgb(0 0 0 / 10%);
+  }
+`;
+
+export const XmarkBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  padding: 15px 20px;
+  margin-bottom: 20px;
+
+  img {
+    width: 16px;
+    height: 16px;
+
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+export const ProfileBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #e9e9e9;
+`;
+
+export const UserInfoBox = styled.div`
+  display: flex;
+  padding: 20px 12px;
+
+  @media ${breakPoints.tablet} {
+    padding: 20px;
+  }
+
+  @media ${breakPoints.web} {
+    padding: 20px;
+  }
+`;
+
+export const ProfileImg = styled.div`
+  img {
+    width: 72px;
+    height: 72px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Name = styled.div`
+  padding: 2px 15px;
+
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  color: #0d223a;
+`;
+
+export const Position = styled.div`
+  padding: 2px 15px;
+
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  color: #999999;
+`;
+
+export const Point = styled.div`
+  padding: 2px 15px;
+
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 21px;
+  color: #0d223a;
+`;
+
+export const LoginBox = styled.div`
+  width: 100%;
+  padding: 48px 12px;
+  border-bottom: 1px solid #e9e9e9;
+
+  span {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 21px;
+    color: #0d223a;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const Option = styled.div`
+  width: 100%;
+  padding: 20px 12px;
+  border-bottom: 1px solid #e9e9e9;
+
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 21px;
+  color: #0d223a;
+
+  :hover {
+    cursor: pointer;
+  }
+
+  @media ${breakPoints.tablet} {
+    padding: 20px 25px;
+  }
+
+  @media ${breakPoints.web} {
+    padding: 20px 25px;
+  }
+`;
