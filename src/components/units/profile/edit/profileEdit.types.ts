721e1160b6e9f3de020b293be7dbc81aabc4534d
godboy4256 <godboy4256@github.com>
2022-04-15 00:25:27 +0900

    확인용

--- src/components/units/profile/edit/profileEdit.types.ts
+++ src/components/units/profile/edit/profileEdit.types.ts
@@ -1,40 +1,40 @@
 import { RefObject } from "react";
 
-export interface IPropsProfileEditContext{
-    isvisible?: ""
-    tendency?: string[];
-    position?: string;
-    interest?: string[];
-    technic?: string[];
-    isAdd?: boolean;
-    isOk?: boolean
-    isTendency?: boolean
-    isPosition?: boolean
-    isInterest?: boolean
-    isTechnic?: boolean
-    area?: number
-    tagInputRef?: RefObject<HTMLInputElement>;
-    onClickChangeInput?: () => void;
-    onClickPushTag?: (name: string) => () => void;
-    onClickEditTendency?: () => void
-    onClickEditPosition?: () => void
-    onClickEditInterest?: () => void
-    onClickEditTechnic?: () => void
-    onClickBasicInfoEdit?: () => void
-    // onClickEditTag?: () => void
-    onClickDeleteTendencyTag?: (name: string) => () => void
-    onClickDeletePositionTag?: () => void
-    onClickDeleteInterestTag?: (name: string) => () => void
-    onClickDeleteTechnicTag?: (name: string) => () => void
-    onClickEditButton?: () => void
-    onClickChangeTagButton?: () => void;
+export interface IPropsProfileEditContext {
+  isvisible?: "";
+  tendency?: string[];
+  position?: string;
+  interest?: string[];
+  technic?: string[];
+  isAdd?: boolean;
+  isOk?: boolean;
+  isTendency?: boolean;
+  isPosition?: boolean;
+  isInterest?: boolean;
+  isTechnic?: boolean;
+  area?: number;
+  tagInputRef?: RefObject<HTMLInputElement>;
+  onClickChangeInput?: () => void;
+  onClickPushTag?: (name: string) => () => void;
+  onClickEditTendency?: () => void;
+  onClickEditPosition?: () => void;
+  onClickEditInterest?: () => void;
+  onClickEditTechnic?: () => void;
+  onClickBasicInfoEdit?: () => void;
+  // onClickEditTag?: () => void
+  onClickDeleteTendencyTag?: (name: string) => () => void;
+  onClickDeletePositionTag?: (name: string) => void;
+  onClickDeleteInterestTag?: (name: string) => () => void;
+  onClickDeleteTechnicTag?: (name: string) => () => void;
+  onClickEditButton?: () => void;
+  onClickChangeTagButton?: () => void;
 }
 
 export interface IPropsTag {
-    bgColor: string;
-    // color: string[];
+  bgColor: string;
+  // color: string[];
 }
 
 export interface ISidebarWrapper {
-    area: number | undefined;
+  area: number | undefined;
 }
